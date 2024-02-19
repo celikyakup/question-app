9d61a2303326596c6f785a1c3370918dceb379d7
Yakup Çelik <yakupcelik1.a@gmail.com>
2024-02-19 16:57:00 +0300

    update

--- src/components/questionComponent/Question.jsx
+++ src/components/questionComponent/Question.jsx
@@ -3,11 +3,13 @@ import { questions } from "../../assets/questions";
 import "./question.css";
 
 const Question = () => {
+  //answer choices
   const choices = ["A", "B", "C", "D"];
+
   const [index, setIndex] = useState(0);
   const [question, setQuestion] = useState("");
   const [answer, setAnswer] = useState("");
-  const [img, setImg] = useState("");
+  const [img, setImg] = useState();
   const [isCorrect, setIsCorrect] = useState(null);
   const [myChoise, setMyChoise] = useState("");
   const myChoicesArray = useRef([]);
@@ -16,45 +18,49 @@ const Question = () => {
   const [choicesVisible, setChoicesVisible] = useState(false);
 
   useEffect(() => {
-    if (myChoise !== "") {
-      setIsCorrect(myChoise === answer);
-      if (isCorrect) {
-        setScore(score + 1);
-      }
-    }
-  }, [myChoise, answer, score, isCorrect]);
-
-  useEffect(() => {
     if (index < questions.length) {
+      //We assign the elements of our array to states
       setQuestion(questions[index].question);
       setAnswer(questions[index].answer);
       setImg(questions[index].media);
       const timerChoices = setTimeout(() => {
+        // Choices become active after 10 second
         setChoicesVisible(true);
       }, 10000);
-  
+
+      if (myChoise !== "") {
+        //It checks whether the option we chose is correct.
+        setIsCorrect(myChoise === answer);
+        if (isCorrect) {
+          //If selected option is correct , the score increases.
+          setScore(score + 1);
+        }
+      }
+
       if (!quizComplete) {
         if (isCorrect !== null) {
+          //increase index
           setIndex((prevIndex) => prevIndex + 1);
           myChoicesArray.current.push(myChoise);
           setMyChoise("");
           setIsCorrect(null);
           setChoicesVisible(false);
         } else if (!quizComplete) {
+          //auto pass when do not any thing
           const timer = setTimeout(() => {
             setIndex((prevIndex) => prevIndex + 1);
             myChoicesArray.current.push("Boş bıraktınız.");
             setMyChoise("");
             setChoicesVisible(false);
           }, 30000);
-          
-          return () => clearTimeout(timer,timerChoices);
+
+          return () => clearTimeout(timer, timerChoices);
         }
       }
     } else {
       setQuizComplete(true);
     }
-  }, [index, isCorrect, myChoise, quizComplete]);
+  }, [answer, index, isCorrect, myChoise, quizComplete, score]);
 
   const handleInput = (event) => {
     setMyChoise(event.target.value);
